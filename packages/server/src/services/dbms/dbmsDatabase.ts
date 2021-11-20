796134d73ff87295dedf4d44a9055339ead29a02
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-11-20 19:23:29 +0200

    feat: add basic `Database` class

---
+++ packages/server/src/services/dbms/dbmsDatabase.ts
@@ -0,0 +1,35 @@
+import DbmsTable from '@services/dbms/dbmsTable';
+import { nanoid } from 'nanoid';
+
+interface TablesIndex {
+  [tableId: string]: DbmsTable;
+}
+
+class DbmsDatabase {
+  public id: string;
+  public name: string;
+  public tablesIndex: TablesIndex;
+
+  constructor(name: string, tables: DbmsTable[]) {
+    this.id = nanoid();
+    this.name = name;
+    this.tablesIndex = tables.reduce((index, table) => {
+      index[table.id] = table;
+      return index;
+    }, {} as TablesIndex);
+  }
+
+  get tables(): DbmsTable[] {
+    return Object.values(this.tablesIndex);
+  }
+
+  public getTable(id: string): DbmsTable | undefined {
+    return this.tablesIndex[id];
+  }
+
+  get tablesAmount(): number {
+    return this.tables.length;
+  }
+}
+
+export default DbmsDatabase;
