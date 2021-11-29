5ad68f735c47e12fcb2843207402aa45fe95a7dd
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-11-29 17:32:32 +0200

    feat: add basic api routes definitions

--- packages/client/src/redux/queries/api_routes.ts
+++ packages/client/src/redux/queries/api_routes.ts
@@ -1,1 +1,14 @@
-export const API = {};
+export const API = {
+  DATABASES: '/databases',
+  DATABASE: (databaseId: string): string => `${API.DATABASES}/${databaseId}`,
+
+  TABLES: (databaseId: string): string => `${API.DATABASE(databaseId)}/tables`,
+  TABLE: (databaseId: string, tableId: string): string => `${API.TABLES(databaseId)}/${tableId}`,
+
+  COLUMNS: (databaseId: string, tableId: string): string => `${API.TABLE(databaseId, tableId)}/columns`,
+  COLUMN: (databaseId: string, tableId: string, columnId: string): string =>
+    `${API.COLUMNS(databaseId, tableId)}/${columnId}`,
+
+  ROWS: (databaseId: string, tableId: string): string => `${API.TABLE(databaseId, tableId)}/rows`,
+  ROW: (databaseId: string, tableId: string, rowId: string): string => `${API.ROWS(databaseId, tableId)}/${rowId}`,
+};
