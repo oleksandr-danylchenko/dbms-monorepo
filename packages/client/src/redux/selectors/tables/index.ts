67372a9f23ee6a0e1c129eef21250c7324fb9a09
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-11-30 16:18:15 +0200

    feat: add selectors for the tables

---
+++ packages/client/src/redux/selectors/tables/index.ts
@@ -0,0 +1,21 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '../../store';
+import { tablesAdapter, tablesInitialState } from '../../queries/tables/tables_cache_helper';
+import { tablesApi } from '../../queries/tables';
+import { selectActiveDatabaseId } from '../application';
+import { undefinedResultSelector } from '../utils';
+
+const selectTablesResult = createSelector(selectActiveDatabaseId, (databaseId) =>
+  !databaseId ? undefinedResultSelector : tablesApi.endpoints.getTables.select({ databaseId })
+);
+
+const selectTablesData = createSelector(
+  (state: RootState) => selectTablesResult(state)(state),
+  (tablesResults) => tablesResults.data
+);
+
+export const {
+  selectAll: selectAllTables,
+  selectById: selectTableById,
+  selectIds: selectTablesIds,
+} = tablesAdapter.getSelectors((state: RootState) => selectTablesData(state) || tablesInitialState);
--- packages/client/src/redux/selectors/utils.ts
+++ packages/client/src/redux/selectors/utils.ts
@@ -1,6 +1,8 @@
 import { Selector } from '@reduxjs/toolkit';
 
-// Allows to pass options object, instead of a few distinct params
+export const undefinedResultSelector = () => ({ data: undefined });
+
+// Allows passing options object, instead of a few distinct params
 // https://cutt.ly/hE5Xe98
 export const createParameterSelector =
   <T, P = Record<string, T>>(selector: Selector<P, T>) =>
