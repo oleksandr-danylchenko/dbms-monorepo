e535ec6ab326365be43e69b750a1d622c4d8e0eb
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-11-30 21:06:16 +0200

    feat: add basic database card view

--- packages/client/src/pages/Databases/Cards/index.tsx
+++ packages/client/src/pages/Databases/Cards/index.tsx
@@ -1,40 +1,50 @@
 import { FC } from 'react';
-import { Card, Header, Segment, Sidebar } from 'semantic-ui-react';
-import styles from './styles.module.scss';
+import { Card, Icon, Menu } from 'semantic-ui-react';
+import { useHistory } from 'react-router';
 import { useAppSelector } from '../../../redux/hooks/app/useAppSelector';
 import { useGetDatabasesQuery } from '../../../redux/queries/databases';
 import { selectAllDatabases } from '../../../redux/selectors/databases';
+import { useAppDispatch } from '../../../redux/hooks/app/useAppDispatch';
+import { updateActiveIds } from '../../../redux/slices/application';
+import { selectActiveDatabaseId } from '../../../redux/selectors/application';
 
 const DatabasesCards: FC = () => {
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+
+  const activeDatabaseId = useAppSelector(selectActiveDatabaseId);
+
   const { isLoading: isDatabasesLoading, error: databasesError } = useGetDatabasesQuery();
   const databases = useAppSelector(selectAllDatabases);
 
+  const handleTableClick = (tableId: string): void => {
+    dispatch(updateActiveIds({ tableId }));
+    history.push(`/databases/${activeDatabaseId}/table/${tableId}`);
+  };
+
   return (
     <Card.Group centered doubling stackable>
-      <Card
-        href="#card-example-link-card"
-        header="Elliot Baker"
-        meta="Friend"
-        description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
-      />
-      <Card
-        href="#card-example-link-card"
-        header="Elliot Baker"
-        meta="Friend"
-        description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
-      />
-      <Card
-        href="#card-example-link-card"
-        header="Elliot Baker"
-        meta="Friend"
-        description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
-      />
-      <Card
-        href="#card-example-link-card"
-        header="Elliot Baker"
-        meta="Friend"
-        description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
-      />
+      {databases.map((database) => (
+        <Card>
+          <Card.Content>
+            <Card.Header>{database.name}</Card.Header>
+            <Card.Meta>{database.id}</Card.Meta>
+            <Card.Description>
+              <Menu vertical fluid>
+                {!Object.values(database.tablesIndex).length && (
+                  <Menu.Item>
+                    <Icon name="dont" />
+                    No tables presented
+                  </Menu.Item>
+                )}
+                {Object.values(database.tablesIndex).map((table) => (
+                  <Menu.Item onClick={() => handleTableClick(table.id)}>{table.id}</Menu.Item>
+                ))}
+              </Menu>
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      ))}
     </Card.Group>
   );
 };
