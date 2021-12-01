fec1f324dbfa97ffba4a527643110fe9f50a5ce7
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-01 14:39:48 +0200

    feat: add data invalidation for the databases

--- packages/client/src/redux/hooks/tables/index.ts
+++ packages/client/src/redux/hooks/tables/index.ts
@@ -3,8 +3,8 @@ import { useGetTablesQuery } from '../../queries/tables';
 import { useActiveDatabase } from '../databases';
 
 export const useActiveDatabaseTables = () => {
-  const { data: activeDatabase, isFetching: isActiveDatabaseFetching } = useActiveDatabase();
-  const isActiveDatabaseReady = activeDatabase && !isActiveDatabaseFetching;
+  const { data: activeDatabase, isLoading: isActiveDatabaseLoading } = useActiveDatabase();
+  const isActiveDatabaseReady = activeDatabase && !isActiveDatabaseLoading;
 
   return useGetTablesQuery(isActiveDatabaseReady ? { databaseId: activeDatabase.id } : skipToken);
 };
--- packages/client/src/redux/queries/tables/index.ts
+++ packages/client/src/redux/queries/tables/index.ts
@@ -4,16 +4,19 @@ import { Table } from '../../../models/dbms';
 import { CreateTableDto, UpdateTableDto } from '../../../dtos';
 import { API } from '../api_routes';
 import { tablesAdapter, tablesInitialState, transformTable, transformTables } from './tables_cache_helper';
+import { providesEntitiesTags, providesTag } from '../../services/cache_tags_generator';
 
 export const tablesApi = dbmsApi.injectEndpoints({
   endpoints: (build) => ({
     getTables: build.query<EntityState<Table>, { databaseId: string }>({
       query: ({ databaseId }) => API.TABLES(databaseId),
       transformResponse: transformTables,
+      providesTags: (result) => providesEntitiesTags(result, 'Table'),
     }),
     getTable: build.query<Table, { databaseId: string; tableId: string }>({
       query: ({ databaseId, tableId }) => API.TABLE(databaseId, tableId),
       transformResponse: transformTable,
+      providesTags: (result) => providesTag(result, 'Table'),
       async onQueryStarted({ databaseId }, { dispatch, queryFulfilled }) {
         const { data: table } = await queryFulfilled;
         dispatch(
@@ -30,6 +33,7 @@ export const tablesApi = dbmsApi.injectEndpoints({
         body: table,
       }),
       transformResponse: transformTable,
+      invalidatesTags: (result) => (result ? [{ type: 'Database', id: result.databaseId }] : []),
       async onQueryStarted({ databaseId }, { dispatch, queryFulfilled }) {
         const { data: newTable } = await queryFulfilled;
         dispatch(
@@ -46,16 +50,24 @@ export const tablesApi = dbmsApi.injectEndpoints({
         body: table,
       }),
       transformResponse: transformTable,
+      invalidatesTags: (result) => (result ? [{ type: 'Database', id: result.databaseId }] : []),
       async onQueryStarted({ databaseId, tableId, table }, { dispatch, queryFulfilled }) {
-        const patchResult = dispatch(
+        const patchTablesResult = dispatch(
           tablesApi.util.updateQueryData('getTables', { databaseId }, (draftTables) => {
             tablesAdapter.updateOne(draftTables || tablesInitialState, { id: tableId, changes: table });
+          })
+        );
+        const patchTableResult = dispatch(
+          tablesApi.util.updateQueryData('getTable', { databaseId, tableId }, (draftTable) => {
+            draftTable.name = table.name;
+            draftTable.columnsOrderIndex = table.columnsOrderIndex;
           })
         );
         try {
           await queryFulfilled;
         } catch {
-          patchResult.undo();
+          patchTablesResult.undo();
+          patchTableResult.undo();
         }
       },
     }),
@@ -66,6 +78,7 @@ export const tablesApi = dbmsApi.injectEndpoints({
           method: 'DELETE',
         };
       },
+      invalidatesTags: (result) => (result ? [{ type: 'Database', id: result.databaseId }] : []),
       async onQueryStarted({ databaseId, tableId }, { dispatch, queryFulfilled }) {
         await queryFulfilled;
         dispatch(
