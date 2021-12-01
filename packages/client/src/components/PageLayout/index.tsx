8ee0eb382e8a053cfa1fc014364763d9de6fa820
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-01 11:46:18 +0200

    feat: add reusable `PageLayout` component

---
+++ packages/client/src/components/PageLayout/index.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactElement } from 'react';
+import { Grid } from 'semantic-ui-react';
+import styles from './styles.module.scss';
+
+interface PageLayoutProps {
+  sidebar: ReactElement;
+  content: ReactElement;
+}
+
+const PageLayout: FC<PageLayoutProps> = ({ sidebar, content }) => (
+  <Grid row={1} columns="equal" padded="vertically" className={styles.PageLayout}>
+    <Grid.Column width={3}>{sidebar}</Grid.Column>
+    <Grid.Column>{content}</Grid.Column>
+  </Grid>
+);
+
+export default PageLayout;
--- packages/client/src/pages/Databases/index.tsx
+++ packages/client/src/pages/Databases/index.tsx
@@ -1,29 +1,8 @@
 import { FC } from 'react';
-import { useHistory } from 'react-router';
-import { Grid, Header, Segment, Sidebar } from 'semantic-ui-react';
-import { useGetDatabasesQuery } from '../../redux/queries/databases';
-import { useAppSelector } from '../../redux/hooks/app/useAppSelector';
-import { selectAllDatabases } from '../../redux/selectors/databases';
-import styles from './styles.module.scss';
 import DatabasesSidebar from './Sidebar';
 import DatabasesCards from './Cards';
+import PageLayout from '../../components/PageLayout';
 
-const Databases: FC = () => {
-  const history = useHistory();
-
-  const { isLoading: isDatabasesLoading, error: databasesError } = useGetDatabasesQuery();
-  const databases = useAppSelector(selectAllDatabases);
-
-  return (
-    <Grid row={1} columns="equal" padded="vertically" className={styles.Databases__Content}>
-      <Grid.Column width={3}>
-        <DatabasesSidebar />
-      </Grid.Column>
-      <Grid.Column>
-        <DatabasesCards />
-      </Grid.Column>
-    </Grid>
-  );
-};
+const Databases: FC = () => <PageLayout sidebar={<DatabasesSidebar />} content={<DatabasesCards />} />;
 
 export default Databases;
