582f8eb4001846329048c1454a50d0004651d074
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-04 17:58:04 +0200

    feat: add UI error handling for the databases

--- packages/client/src/pages/Databases/ModifyModal/index.tsx
+++ packages/client/src/pages/Databases/ModifyModal/index.tsx
@@ -1,11 +1,12 @@
-import { ChangeEvent, FC, useMemo, useState } from 'react';
-import { Form } from 'semantic-ui-react';
+import { FC, useMemo } from 'react';
+import { Form, Message } from 'semantic-ui-react';
 import { BindingAction } from '../../../models/functions';
 import { useAppSelector } from '../../../redux/hooks/app/useAppSelector';
 import { selectDatabaseById } from '../../../redux/selectors/databases';
 import ModifyModal from '../../../components/ModifyModal';
 import { useUpdateDatabaseMutation } from '../../../redux/queries/databases';
 import { UpdateDatabaseDto } from '../../../dtos';
+import { useFormState } from '../../../hooks/useFormState';
 
 interface DatabaseModifyModalProps {
   databaseId: string;
@@ -16,39 +17,42 @@ const DatabaseModifyModal: FC<DatabaseModifyModalProps> = ({ databaseId, onClose
   const modifyingDatabase = useAppSelector((state) => selectDatabaseById(state, databaseId));
   const [updateDatabase, { isLoading: isUpdateLoading, error: updateError }] = useUpdateDatabaseMutation();
 
-  const [databaseFormState, setDatabaseFormState] = useState<UpdateDatabaseDto>({
+  const [databaseFormState, handleDatabaseFormChange] = useFormState<UpdateDatabaseDto>({
     name: modifyingDatabase?.name || '',
   });
-  const handleFormChange = (
-    _: ChangeEvent<HTMLInputElement>,
-    { name, value }: { name: keyof UpdateDatabaseDto; value: string }
-  ): void => {
-    setDatabaseFormState({ [name]: value });
-  };
 
   const handleSaveDatabase = (): void => {
     const updatedDatabase: UpdateDatabaseDto = {
       ...databaseFormState,
     };
+
+    // Ignore if nothing has changed
+    if (updatedDatabase.name === modifyingDatabase?.name) {
+      onClose();
+      return;
+    }
+
     updateDatabase({ databaseId, database: updatedDatabase })
       .unwrap()
       .then(() => onClose());
   };
 
   const databaseForm = useMemo(() => {
+    const updateFetchError = updateError as { status: number; data: { message: string } };
     return (
-      <Form loading={isUpdateLoading}>
+      <Form loading={isUpdateLoading} error={!!updateFetchError}>
         <Form.Input
           name="name"
           label="Name"
           placeholder="Enter database name"
           required
           value={databaseFormState.name}
-          onChange={handleFormChange as any}
+          onChange={handleDatabaseFormChange as any}
         />
+        <Message error header={updateFetchError?.status || ''} content={updateFetchError?.data?.message || ''} />
       </Form>
     );
-  }, [databaseFormState.name, isUpdateLoading]);
+  }, [databaseFormState.name, handleDatabaseFormChange, isUpdateLoading, updateError]);
 
   return (
     <ModifyModal
