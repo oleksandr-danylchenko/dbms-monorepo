19b79e4055aa679b7137041aa3af1ce62128aa28
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-04 14:00:51 +0200

    feat: add deletion of database

--- packages/client/src/pages/Databases/index.tsx
+++ packages/client/src/pages/Databases/index.tsx
@@ -1,23 +1,50 @@
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useState } from 'react';
 import DatabasesSidebar from './Sidebar';
 import DatabasesCards from './Cards';
 import PageLayout from '../../components/PageLayout';
+import DeleteModal from '../../components/DeleteModal';
+import { useAppSelector } from '../../redux/hooks/app/useAppSelector';
+import { selectDatabaseById } from '../../redux/selectors/databases';
+import { useDeleteDatabaseMutation } from '../../redux/queries/databases';
 
 const Databases: FC = () => {
+  const [selectedDatabaseId, setSelectedDatabaseId] = useState<string>();
+  const selectedDatabase = useAppSelector((state) => selectDatabaseById(state, selectedDatabaseId || ''));
+
+  const [deleteDatabase] = useDeleteDatabaseMutation();
+
   const handleDatabaseEditClick = useCallback(({ databaseId }: { databaseId: string }) => {
-    console.log('Editing', databaseId);
+    setSelectedDatabaseId(databaseId);
   }, []);
 
   const handleDatabaseDeleteClick = useCallback(({ databaseId }: { databaseId: string }) => {
-    console.log('Deleting', databaseId);
+    setSelectedDatabaseId(databaseId);
+  }, []);
+
+  const handleDatabaseDeletePermitClick = useCallback(() => {
+    if (!selectedDatabaseId) return;
+    deleteDatabase({ databaseId: selectedDatabaseId });
+    setSelectedDatabaseId(undefined);
+  }, [deleteDatabase, selectedDatabaseId]);
+
+  const handleDatabaseCancelClick = useCallback(() => {
+    setSelectedDatabaseId(undefined);
   }, []);
 
   return (
-    <PageLayout
-      header="Databases"
-      sidebar={<DatabasesSidebar />}
-      content={<DatabasesCards onEditClick={handleDatabaseEditClick} onDeleteClick={handleDatabaseDeleteClick} />}
-    />
+    <>
+      <PageLayout
+        header="Databases"
+        sidebar={<DatabasesSidebar />}
+        content={<DatabasesCards onEditClick={handleDatabaseEditClick} onDeleteClick={handleDatabaseDeleteClick} />}
+      />
+      <DeleteModal
+        open={!!selectedDatabaseId}
+        entityName={`database ${selectedDatabase?.name}`}
+        onCancel={handleDatabaseCancelClick}
+        onPermit={handleDatabaseDeletePermitClick}
+      />
+    </>
   );
 };
 
