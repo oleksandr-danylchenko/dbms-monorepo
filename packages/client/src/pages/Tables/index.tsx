1551eb9641b370cdfcbe25c2632725eaa26b209a
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-04 19:53:36 +0200

    feat: add tables modals to the /tables page

--- packages/client/src/pages/Tables/index.tsx
+++ packages/client/src/pages/Tables/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import PageLayout from '../../components/PageLayout';
 import TablesSidebar from './Sidebar';
@@ -6,9 +6,16 @@ import { useAppDispatch } from '../../redux/hooks/app/useAppDispatch';
 import { updateActiveIds } from '../../redux/slices/application';
 import TablesCards from './Cards';
 import { useActiveDatabase } from '../../redux/hooks/databases';
+import TableDeleteModal from './DeleteModal';
+import TableCreateModal from './CreateModal';
+import TableModifyModal from './ModifyModal';
 
 const Tables: FC = () => {
   const dispatch = useAppDispatch();
+
+  const [isCreatingTable, setCreatingTable] = useState(false);
+  const [editTableId, setEditTableId] = useState<string>();
+  const [deleteTableId, setDeleteTableId] = useState<string>();
 
   const { databaseId: paramsDatabaseId } = useParams<{ databaseId: string }>();
   const { data: activeDatabase } = useActiveDatabase();
@@ -18,12 +25,17 @@ const Tables: FC = () => {
   }, [dispatch, paramsDatabaseId]);
 
   return (
-    <PageLayout
-      header={<>Tables {activeDatabase?.name && `for ${activeDatabase.name}`}</>}
-      backLink="/databases"
-      sidebar={<TablesSidebar />}
-      content={<TablesCards />}
-    />
+    <>
+      <PageLayout
+        header={<>Tables {activeDatabase?.name && `for ${activeDatabase.name}`}</>}
+        backLink="/databases"
+        sidebar={<TablesSidebar />}
+        content={<TablesCards />}
+      />
+      {isCreatingTable && <TableCreateModal onClose={() => setCreatingTable(false)} />}
+      {editTableId && <TableModifyModal tableId={editTableId} onClose={() => setEditTableId(undefined)} />}
+      {deleteTableId && <TableDeleteModal tableId={deleteTableId} onClose={() => setDeleteTableId(undefined)} />}
+    </>
   );
 };
 
