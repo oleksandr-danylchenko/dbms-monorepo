2d68351814e49a4ec4340759485b323c22eadbe6
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-05 20:58:52 +0200

    feat!: add `orderIndex` to the column directly

--- packages/server/src/models/dbms/column.ts
+++ packages/server/src/models/dbms/column.ts
@@ -6,13 +6,27 @@ class Column {
   private readonly _id: string;
   private _name: string;
   private readonly _tableId: string;
+  private _orderIndex: number;
   private _type: FieldType;
   private _validator: Validator;
 
-  constructor({ id, name, tableId, type }: { id?: string; name: string; tableId: string; type: FieldType }) {
+  constructor({
+    id,
+    name,
+    type,
+    tableId,
+    orderIndex,
+  }: {
+    id?: string;
+    name: string;
+    type: FieldType;
+    tableId: string;
+    orderIndex: number;
+  }) {
     this._id = id || nanoid();
     this._name = name;
     this._tableId = tableId;
+    this._orderIndex = orderIndex;
     this._type = type;
     this._validator = DbmsValidation.getValidatorByType(type);
   }
@@ -27,6 +41,14 @@ class Column {
 
   public set name(value: string) {
     this._name = value;
+  }
+
+  public get orderIndex() {
+    return this._orderIndex;
+  }
+
+  public set orderIndex(value: number) {
+    this._orderIndex = value;
   }
 
   public get tableId() {
