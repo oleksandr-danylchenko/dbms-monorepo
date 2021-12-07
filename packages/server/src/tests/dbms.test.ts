2d0559838122a4a38913a6a3cb901925baa2ca5d
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-12-07 22:34:42 +0200

    feat: add test for the error POST /databases endpoint

--- packages/server/src/tests/dbms.test.ts
+++ packages/server/src/tests/dbms.test.ts
@@ -3,6 +3,7 @@ import App from '@/app';
 import DbmsRoute from '@routes/dbms.route';
 import DbmsService from '@services/dbms/dbms.service';
 import DatabaseMapper from '@/mappers/database.mapper';
+import { CreateDatabaseDto } from '@dtos/database.dto';
 
 afterAll(async () => {
   await new Promise<void>((resolve) => setTimeout(() => resolve(), 100));
@@ -57,6 +58,30 @@ describe('Testing Databases', () => {
         .get(`${dbmsRoute.path}/${firstDatabaseDto.id}`)
         .expect('Content-Type', /json/)
         .expect(200, { data: firstDatabaseDto, message: 'findDatabaseById' }, done);
+    });
+  });
+
+  describe('[POST] /databases', () => {
+    it('response Create database duplicated name', (done) => {
+      const dbmsRoute = new DbmsRoute();
+      const app = new App([dbmsRoute]);
+
+      const dbmsService = new DbmsService();
+      const databases = dbmsService.allDatabases;
+      if (!databases.length) {
+        done('No databases have been created yet');
+      }
+
+      const firstDatabase = databases[0];
+      const databaseData: CreateDatabaseDto = {
+        name: firstDatabase.name,
+      };
+
+      request(app.getServer())
+        .post(`${dbmsRoute.path}`)
+        .send(databaseData)
+        .expect('Content-Type', /json/)
+        .expect(409, done);
     });
   });
 });
